c7f113fd70f413204ab11e5151fdca185f001da2
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-01-24 13:30:02 +0100

    Revert "Merge branch 'heroku-dota' into dota"
    This reverts commit 3b392d7d74ae6aaec39d3dc79d53626cd3a12045.
    
    Reason: I fucked up.

--- Procfile
+++
@@ -1,1 +0,0 @@
-bot: node ./lib/index.js
--- config.js
+++ config.js
@@ -90,6 +90,13 @@ module.exports = {
 		event_name: "Inhouse"
 	},
 
+	db: {
+		"user": "",
+		"password": "",
+		"host": "",
+		"database": ""
+	},
+
 	steam: {
 		// The name that the Steam bots will take. It will be appended with "#id" as in "#1".
 		name: "ScheduleBot",
