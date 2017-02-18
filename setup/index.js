21515528ffb827a99a60b673e7514ed854e3c8db
Pablo Rodríguez <pabloviolin8@gmail.com>
2017-02-18 20:16:45 +0100

    wip

--- setup/index.js
+++ setup/index.js
@@ -1,30 +1,58 @@
 "use strict";
 
-const inquirer = require("inquirer")
+const Heroku   = require("heroku-client")
+	, inquirer = require("inquirer")
 	, fs       = require("fs")
 	, path     = require("path")
 	, pg       = require("pg")
+	, readEnv  = require("./readEnv")
 	, server   = require("schedulebot-setup");
 
 const port = process.env.PORT || 3000;
 
 let detailsStructure = require("../scripts/shared/db-details")({});
+let dbValues, projectRoot = path.join(__dirname, ".."), envExists = fs.existsSync(projectRoot + "/.ENV");
 
 if (process.env.DATABASE_URL) {
+	// We'll think that DATABASE_URL being set means that the code is running on Heroku.
 	run(process.env.DATABASE_URL, true);
 } else {
+	// If the DB details are saved on an .ENV file, load them
+	// Otherwise ask them and save them
+	if (envExists) {
+
+		let envFile = readEnv(projectRoot);
+
+		if (envFile !== null) {
+
+			run(envFile.url, envFile.useSSL);
+
+		} else {
+
+			askDbCredentials();
+
+		}
+
+	} else {
+
+		askDbCredentials();
+
+	}
+}
+
+function askDbCredentials() {
+
 	console.log("Hello! Welcome to the ScheduleBot setup server.\n" +
 		"Please enter your PostgreSQL database credentials.\n" +
 		"Then, you will be able to visit the Setup server on http://localhost:" + port + "\n");
 
 	inquirer.prompt(detailsStructure).then(values => {
 
-		let connStr = "postgres://" + values.db_user + ":" + values.db_password
-			+ "@" + values.db_host + "/" + values.db_database + "";
-
-		run(connStr, values.db_ssl);
+		dbValues = values;
+		run(generateDatabaseUrl(values), values.db_ssl);
 
 	}).catch(console.error);
+
 }
 
 function run(connStr, ssl) {
@@ -39,6 +67,10 @@ function run(connStr, ssl) {
 		if (!err) {
 
 			let existingData = {};
+
+			if (!envExists) {
+				saveEnv(dbValues);
+			}
 
 			dbStructureExists(db)
 				.then(exists => {
@@ -55,7 +87,15 @@ function run(connStr, ssl) {
 
 				})
 				.then(() => server.run(port, existingData))
-				.then(console.log)
+				.then(result => afterRun(db, result))
+				.then(() => {
+
+					console.log("All good! You may now run the bot with:\n\n" +
+
+						"npm run bot");
+					process.exit(0);
+
+				})
 				.catch(err => {
 
 					console.error("An unexpected error occurred!\n");
@@ -72,6 +112,31 @@ function run(connStr, ssl) {
 
 	});
 
+}
+
+function afterRun(client, result) {
+	return new Promise((fulfill, reject) => {
+		if (result.heroku) {
+
+			const hk = new Heroku({ token: result.heroku.key });
+
+			hk.patch(`/apps/${result.heroku.app}/formation/web`, {
+				quantity: 0
+			})
+				.then(() => applyNewConfig(client, result))
+				.then(() => hk.patch(`/apps/${result.heroku.app}/formation/bot`, {
+					quantity: 1
+				}))
+				.then(() => console.log("\nAll good! The setup page should shut down, and the " +
+					"bot should boot automatically."))
+				.catch(reject);
+
+		} else {
+
+			applyNewConfig(client, result).then(fulfill).catch(reject);
+
+		}
+	});
 }
 
 function dbStructureExists(client) {
@@ -113,7 +178,7 @@ function createDbStructure(client) {
 function getConfigFromDb(client) {
 	return new Promise((fulfill, reject) => {
 	    client.query(
-	    	"SELECT config.bot_token, config.config, admins.userid AS adminid " +
+	    	"SELECT config.bot_token, config.settings, admins.userid AS adminid " +
 			"FROM config INNER JOIN admins ON 1=1 LIMIT 1;" +
 			"SELECT * FROM steam_bots",
 			(err, result) => {
@@ -140,7 +205,7 @@ function parseConfig(rows) {
 	if (rows[0]) {
 		result.discord.bot.token = rows[0].bot_token;
 		result.discord.admin.id = rows[0].adminid;
-		result.settings = rows[0].config;
+		result.settings = rows[0].settings;
 
 		if (rows[1]) {
 			for (let i = 1; i < rows.length; i++) {
@@ -155,4 +220,154 @@ function parseConfig(rows) {
 	}
 
 	return result;
+}
+
+function applyNewConfig(client, newConfig) {
+	return new Promise((fulfill, reject) => {
+
+		let updateConfigPromise = new Promise((fulfill2, reject2) => {
+
+			runQuery(client, "SELECT * FROM public.config").then(result => {
+
+				if (result.rowCount === 0) {
+
+					runQuery(
+						client,
+						"INSERT INTO public.config (bot_token, settings) " +
+						"VALUES ($1, $2)",
+						newConfig.discord.bot.token,
+						newConfig.settings
+					).then(() => fulfill2()).catch(reject2);
+
+				} else {
+
+					runQuery(
+						client,
+						"INSERT INTO public.config (bot_token, settings) " +
+						"VALUES ($1, $2)",
+						newConfig.discord.bot.token,
+						newConfig.settings
+					).then(() => fulfill2()).catch(reject2);
+
+				}
+
+			}).catch(reject2);
+
+		});
+
+		let updateAdminPromise = new Promise((fulfill2, reject2) => {
+
+			runQuery(
+				client,
+				"SELECT userid FROM public.admins WHERE userid = $1",
+				newConfig.discord.admin.id
+
+			).then(result => {
+
+				if (result.rowCount === 0) {
+
+					runQuery(
+						client,
+						"INSERT INTO public.admins (userid, name) VALUES ($1, $2)",
+						newConfig.discord.admin.id,
+						newConfig.discord.admin.username
+					).then(() => fulfill2()).catch(reject2);
+
+				} else {
+
+					fulfill();
+
+				}
+
+			}).catch(reject2);
+
+		});
+
+		let updateSteamBotsPromise = new Promise((fulfill2, reject2) => {
+
+			let promises = [];
+
+			for (let bot of newConfig.steamBots) {
+
+				promises.push(new Promise((fulfill3, reject3) => {
+
+					runQuery(
+						client,
+						"SELECT id FROM public.steam_bots WHERE username = $1",
+						bot.username
+					).then(result => {
+
+						if (result.rowCount === 0) {
+
+							runQuery(
+								client,
+								"INSERT INTO public.steam_bots (username, password, steam_guard, " +
+								"steam_guard_code) VALUES ($1, $2, $3, $4)",
+								bot.username,
+								bot.password,
+								bot.steamGuardEnabled,
+								bot.steamGuardCode
+							).then(() => fulfill3()).catch(reject3);
+
+						} else {
+							fulfill3();
+						}
+
+					}).catch(reject3);
+
+				}));
+
+			}
+
+			Promise.all(promises).then(() => fulfill2()).catch(reject2);
+
+		});
+
+		Promise.all([
+			updateConfigPromise,
+			updateAdminPromise,
+			updateSteamBotsPromise
+		]).then(() => fulfill()).catch(reject);
+
+	});
+}
+
+function runQuery(client, query, ...data) {
+	return new Promise((fulfill, reject) => {
+
+		if (data) {
+
+			client.query(query, data, (err, result) => {
+				if (err) {
+					reject(err);
+				} else {
+					fulfill(result);
+				}
+			});
+
+		} else {
+
+			client.query(query, (err, result) => {
+				if (err) {
+					reject(err);
+				} else {
+					fulfill(result);
+				}
+			});
+
+		}
+
+	});
+}
+
+function saveEnv(dbDetails) {
+	let file = "DATABASE_URL=" + generateDatabaseUrl(dbDetails);
+	file += "\nDATABASE_USE_SSL=" + dbDetails.db_ssl;
+
+	fs.writeFileSync(projectRoot + "/.ENV", file, { encoding: "utf-8" });
+}
+
+function generateDatabaseUrl(dbDetails) {
+	return "postgres://" + dbDetails.db_user + ":" + dbDetails.db_password
+		+ "@" + dbDetails.db_host + "/" + dbDetails.db_database + "";
 }
